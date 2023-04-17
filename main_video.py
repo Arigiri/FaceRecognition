cf121b69629c32a402cc00e0ec192b134d67678c
Arigiri <63282838+Arigiri@users.noreply.github.com>
2023-04-17 11:15:23 +0700

    This can work

--- main_video.py
+++ main_video.py
@@ -1,29 +1,35 @@
 import cv2
 from simple_facerec import SimpleFacerec
 import tkinter as tk
-
+import face_recognition
+import os
 
 # Encode faces from a folder
 sfr = SimpleFacerec()
-sfr.load_encoding_images("images/")
+# sfr.load_encoding_images("images/")
+sfr.load_saved_encoding()
 # Create root for name input bar
 # root = tk.Tk()
 # Load Camera
 cap = cv2.VideoCapture(0)
 frameLast = ""
 ButtonClicked = False
+name = ""
 def MyClick():
-    global ButtonClicked, root, frame, e
+    global ButtonClicked, root, frame, e, name
     name = e.get()
-    path = "images/" + name + ".png"
-    cv2.imwrite(path, frame)
-    sfr.load_encoding_images(path)
     root.destroy()
-
+    path = "images/" + name + '.png'
+    cv2.imwrite(path, frame)
+    sfr.encode_a_face(path, name)
+    # cv2.imwrite(path, frame)
+    # sfr.load_encoding_images(path)
+    
 def Detect(frame):
 
     # Detect Faces
     face_locations, face_names = sfr.detect_known_faces(frame)
+    # print(face_locations, face_names)
     for face_loc, name in zip(face_locations, face_names):
         y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
 
@@ -35,11 +41,24 @@ def Detect(frame):
 
     key = cv2.waitKey()
     cv2.destroyAllWindows()
-    
 
+
+def DetectFace(frame, draw = True):
+    # rgbframe = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    face_location = face_recognition.face_locations(frame)
+    if draw == False:
+        return frame
+    if len(face_location) != 1:
+        return frame
+    for (top, right, bot, left) in face_location:
+        cv2.rectangle(frame, (left, top), (right, bot), (255, 0, 0))
+        frame = frame[top : bot, left: right]
+    return frame
+    # return frame
 
 while True:
     ret, frame = cap.read()
+    frame = cv2.flip(frame, 1)
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
     if key == 27:
@@ -54,5 +73,7 @@ while True:
         myButton.pack()
         
         root.mainloop()
+    if key == ord('1'):
+        sfr.encode_a_face("images/Minh le")
         # cv2.imwrite("")
 
--- simple_facerec.py
+++ simple_facerec.py
@@ -12,6 +12,16 @@ class SimpleFacerec:
         # Resize frame for a faster speed
         self.frame_resizing = 0.25
 
+    def load_saved_encoding(self):
+        path = os.path.join("images", "EncodingFiles")
+        images_path = glob.glob(os.path.join(path, "*.*"))
+        print("{} encoding images found.".format(len(images_path)))
+        for encode_path in images_path:
+            encode = np.load(encode_path)
+            (filename, ext) = os.path.splitext(os.path.basename(encode_path))
+            self.known_face_encodings.append(encode)
+            self.known_face_names.append(filename)
+    
     def load_encoding_images(self, images_path):
         """
         Load encoding images from path
@@ -22,21 +32,27 @@ class SimpleFacerec:
         images_path = glob.glob(os.path.join(images_path, "*.*"))
 
         print("{} encoding images found.".format(len(images_path)))
-
+        path = os.path.join("images", "EncodingFiles")
+        try:
+            os.mkdir(path)
+        except:
+            pass
         # Store image encoding and names
         for img_path in images_path:
             img = cv2.imread(img_path)
+            print(img_path)
             rgb_img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
             # Get the filename only from the initial file path.
             basename = os.path.basename(img_path)
             (filename, ext) = os.path.splitext(basename)
             # Get encoding
-            img_encoding = face_recognition.face_encodings(rgb_img)[0]
-
+            img_location = face_recognition.face_locations(rgb_img, 2)
+            img_encoding = face_recognition.face_encodings(rgb_img, img_location)[0]
             # Store file name and file encoding
             self.known_face_encodings.append(img_encoding)
             self.known_face_names.append(filename)
+            np.save(os.path.join(path, filename + ".npy"), img_encoding)
         print("Encoding images loaded")
 
     def detect_known_faces(self, frame):
@@ -44,13 +60,14 @@ class SimpleFacerec:
         # Find all the faces and face encodings in the current frame of video
         # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
         rgb_small_frame = cv2.cvtColor(small_frame, cv2.COLOR_BGR2RGB)
-        face_locations = face_recognition.face_locations(rgb_small_frame)
+        face_locations = face_recognition.face_locations(rgb_small_frame, 2)
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         face_names = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(self.known_face_encodings, face_encoding)
+            print(matches)
             name = "Unknown"
 
             # # If a match was found in known_face_encodings, just use the first one.
@@ -69,3 +86,16 @@ class SimpleFacerec:
         face_locations = np.array(face_locations)
         face_locations = face_locations / self.frame_resizing
         return face_locations.astype(int), face_names
+    
+    def encode_a_face(self, path_to_face, name):
+        face = cv2.imread(path_to_face)
+        face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+        face_location = face_recognition.face_locations(face, 2)
+        path = os.path.join("images", "EncodingFiles")
+        try:
+            face_encode = face_recognition.face_encodings(face, face_location)[0]
+            self.known_face_encodings.append(face_encode)
+            self.known_face_names.append(name)
+            np.save(os.path.join(path, name + ".npy"), face_encode)
+        except:
+            print("Cannot detect any face")
